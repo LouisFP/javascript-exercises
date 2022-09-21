d7dfbf3cc09e5badb6232eefdb0d7b0452b5b138
louisfp <louisjfp@gmail.com>
2022-09-21 23:11:00 +0100

    Add and complete exercises 1,2,3,4,5 for JS_ex

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function (val1, val2) {
+  let sum = 0;
+  if (Number.isInteger(val1) && Number.isInteger(val2)) {
+    if (val1 < 0 || val2 < 0) {
+      return "ERROR";
+    } else if (val1 - val2 < 0) {
+      for (let i = val1; i <= val2; i++) {
+        sum += i;
+      }
+      return sum;
+    } else if (val2 - val1 < 0) {
+      for (let i = val2; i <= val1; i++) {
+        sum += i;
+      }
+      return sum;
+    }
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line

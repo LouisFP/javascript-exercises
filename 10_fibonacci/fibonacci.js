a3bf3e48584df4e1d583e937048255437ece9f23
louisfp <louisjfp@gmail.com>
2022-09-28 16:45:41 +0100

    Finish exercises 10,11,12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,26 @@
-const fibonacci = function() {
-
-};
-
+const fibonacci = function(num) {
+    if (Number.isInteger(num) && num > 0) {
+      if (num === 1) {
+        return 1;
+      }
+      if (num === 2) {
+        return 1;
+      } else {
+        return fibonacci(num - 1) + fibonacci(num - 2);
+      }
+    } else if (typeof(num) === "string") {
+      let numNormalise = parseInt(num);
+      if (Number.isInteger(numNormalise)) {
+        if (numNormalise === 1) {
+        return 1;
+      }
+      if (numNormalise === 2) {
+        return 1;
+      } else {
+        return fibonacci(numNormalise - 1) + fibonacci(numNormalise - 2);
+      }
+      } else {return "OOPS";}
+    } else {return 'OOPS';}
+  };
 // Do not edit below this line
 module.exports = fibonacci;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  return array.reduce((oldest, crentPerson) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(crentPerson.yearOfBirth,crentPerson.yearOfDeath);
+    return oldestAge < currentAge ? crentPerson : oldest;
+  })
+};
 
+const getAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
 };
 
 // Do not edit below this line
